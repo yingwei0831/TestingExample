ac6c378fde2c9e001e47cd4e3c5d73be4cc376c7
yingwei0831 <yingwei0831@163.com>
2016-11-29 18:36:14 +0800

    first commit

---
+++ app/src/main/java/com/example/testing/testingexample/Calculator.java
@@ -0,0 +1,22 @@
+package com.example.testing.testingexample;
+
+/**
+ * Created by jiahe008 on 2016/11/29.
+ */
+public class Calculator {
+    public double sum(double a, double b){
+        return a + b;
+    }
+
+    public double substract(double a, double b){
+        return a - b;
+    }
+
+    public double divide(double a, double b){
+        return a / b;
+    }
+
+    public double multiply(double a, double b){
+        return a * b;
+    }
+}
